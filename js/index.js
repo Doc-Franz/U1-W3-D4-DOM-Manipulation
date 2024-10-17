4e4a01500d112f64f7a7f86aa2a808487349adea
Federico Franzi <federico.franzi94@gmail.com>
2024-10-17 16:46:11 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Generare le celle
+
+const numberOfCells = 76;
+const totalNumbers = [];
+
+const createCells = () => {
+  const tombola = document.getElementById("tombola");
+
+  for (let i = 0; i < numberOfCells; i++) {
+    const cellNumber = document.createElement("div");
+    cellNumber.className = "cell";
+
+    const cellNumber1 = document.createElement("h1");
+    cellNumber1.innerText = i + 1;
+    totalNumbers.push(cellNumber);
+
+    tombola.appendChild(cellNumber);
+    cellNumber.appendChild(cellNumber1);
+  }
+};
+
+console.log(totalNumbers);
+
+// Generazione di un numero random
+
+const btn = document.querySelector("#random-number");
+let previousNumber = 0;
+const alreadyExtractedNumbers = [];
+
+btn.onclick = function (e) {
+  const numberSelected = Math.floor(Math.random() * numberOfCells);
+
+  const previousCell = totalNumbers[previousNumber];
+  previousCell.style.color = "black";
+
+  const selectedCell = totalNumbers[numberSelected];
+  selectedCell.style.color = "red";
+  selectedCell.style.backgroundColor = "white";
+  previousNumber = numberSelected;
+};
+
+// Caricamento della pagina
+
+window.onload = () => {
+  createCells();
+  //   selectRandomNumber();
+};
